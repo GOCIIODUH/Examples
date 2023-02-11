5233275a9cc78322b544e83a436ed1774bf8db97
Andrey Perevertov <gociioduh@gmail.com>
2023-02-11 23:50:39 +0300

    examples 005

---
+++ Examples005_Condicion/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    System.Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    System.Console.Write("Привет, ");
+    System.Console.WriteLine(username);
+}
